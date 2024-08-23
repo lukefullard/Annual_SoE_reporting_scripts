441da0e29b7b9b6abfd12d5e5fe2add612aafccb
Sivee Chawla <Sivee.Chawla@horizons.govt.nz>
2024-08-23 14:58:21 +1200

    Update Main manage sites with spatial join

--- biodiversity/scripts/managed_sites/main_managed_sites.py
+++ biodiversity/scripts/managed_sites/main_managed_sites.py
@@ -28,10 +28,62 @@ def load_geodb  (settings:  dict,
         gdf = gpd.read_file(settings.get("gdb_file"), layer = layeri)
         gdb_dict.update({layeri : gdf})
     
-    return gdb_dict    
+    
+    return gdb_dict 
+    
+def getFMUFlie (settings: dict,
+                
+                ):
+    '''
+    
+
+    Parameters
+    ----------
+    settings : dict
+        DESCRIPTION.
+     : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''    
+    #get shape file by reading 
+    FMUshpdf= gpd.read_file(settings.get("FMUShpFile"))
+    
+    return FMUshpdf
+
         
-        
-     
+def  spJoin_GetFMU (gdfi   ,
+                    FMUshpi 
+                    ) :
+   '''
+    Purpose of the function is to perform spatial join between two dataframes e.g. FMU and biodiversity layer. 
+
+    Parameters
+    ----------
+    settings : dict
+        DESCRIPTION. get geodatabase and re
+     : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+  '''
+   # Ensure both GeoDataFrames have the CRS (NZTM from fMU shape file in this case)
+   if gdfi.crs != FMUshpi.crs:
+    gdfi.crs = gdfi.crs.to_crs(FMUshpi.crs)
+    
+   #spatial join fo all the records 
+   FMU_ = gpd.sjoin_nearest(gdfi,FMUshpi,how='left', distance_col='distance_to_nearest')
+   # FMUlevelDf_join    = gpd.sjoin(gdfi, FMUshpi, how='left',predicate="contains")
+   
+   #select records which have nan's in FMU coloumn because they lie on boundary 
+   
+   return FMUlevelDf_nearest
 
 
 #################################################################################################################
@@ -44,12 +96,17 @@ def main():
     #load geodata
     dict_sitelayers  = load_geodb(settings)
     
-    #load gdf of interest here 
+    #load gdf of interest here - this will give us the first layer in the list. 
     gdf = dict_sitelayers.get(
                               settings.get("layers")[0]
                              )
-    
+    FMUshp = getFMUFlie (settings)
+    # FMUshp = dict_sitelayers.get(
+    #                              settings.get("FMUShpFile "))
+    FMUlevel_sites= spJoin_GetFMU(gdf,FMUshp)
     #0.1) Get the Information at FMU level - spatial join 
+    
+    
     #1) Histogram plot of number of managed and the area of manages sites per FMU 
     #2) Histogram plot of "system" type for each FMU 
     #3) Histogram plot of number of sites under a "managed level" for each FMU / the whole region over time. 
